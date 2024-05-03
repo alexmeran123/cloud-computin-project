4ecf7abb256939a8a03445811c699938355a4dac
alexmeran123 <alexandru.meran@gmail.com>
2024-05-03 20:37:17 +0300

    create application

---
+++ client/src/pages/Profile/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Tabs} from 'antd'
+import Projects from "./Projects";
+
+function Profile() {
+    return (<Tabs defaultActiveKey="1">
+        <Tabs.TabPane tab="Projects" key="1">
+            <Projects/>
+        </Tabs.TabPane>
+    </Tabs>)
+}
+
+export default Profile
---
+++ server/server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+require('dotenv').config()
+app.use(express.json())
+const port = process.env.PORT || 5000
+
+const dbConfig = require('./config/dbConfig')
+const usersRoute = require("./routes/usersRoute");
+const projectsRoute = require("./routes/projectsRoute");
+const tasksRoute = require("./routes/tasksRoute");
+
+const path = require("path");
+__dirname = path.resolve();
+
+app.use("/api/users", usersRoute);
+app.use("/api/projects", projectsRoute);
+app.use("/api/tasks", tasksRoute);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/client/build')));
+
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    );
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running....');
+    });
+}
+
+app.listen(port, () => console.log(`Node server listening on port ${port}`))
+
